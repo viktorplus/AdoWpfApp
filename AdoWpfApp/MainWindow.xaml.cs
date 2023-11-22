5e487e813c10818c0458462fc3ed96d866d507f5
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-23 00:34:10 +0100

    1

---
+++ AdoWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DBEntity;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdoWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DatabaseManager databaseManager;
+        const string ConnectionString = @"Data Source=HomeDE\SQLEXPRESS;Initial Catalog=Warehouse;Integrated Security=True;Encrypt=False";
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            databaseManager = new DatabaseManager(ConnectionString);
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //DG_Table.ItemsSource = await databaseManager.ExecuteReaderProducts();
+        }
+}
---
+++ DBEntity/DatabaseManager.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DBEntity
+{
+    public class DatabaseManager
+    {
+
+        // Строка подключения к базе данных
+        public string ConnectionString { get; set; }
+
+        // Объект для работы с соединением с базой данных
+        private SqlConnection sqlConnection { get; set; }
+
+        // Конструктор класса, принимающий строку подключения и инициализирующий объект SqlConnection
+        public DatabaseManager(string connectionString)
+        {
+            ConnectionString = connectionString;
+            sqlConnection = new SqlConnection(ConnectionString);
+        }
+
+        // Метод для открытия соединения с базой данных (асинхронная версия)
+        public bool OpenConnection()
+        {
+            try
+            {
+                // Открываем соединение асинхронно
+                sqlConnection.OpenAsync();
+                return true;
+            }
+            catch (SqlException sql_ex)
+            {
+                // Если произошла ошибка при открытии соединения, бросаем исключение с сообщением об ошибке
+                throw new Exception(sql_ex.Message);
+            }
+        }
+
+        // Метод для закрытия соединения с базой данных (асинхронная версия)
+        public bool CloseConnection()
+        {
+            try
+            {
+                // Закрываем соединение асинхронно
+                sqlConnection.CloseAsync();
+                return true;
+            }
+            catch (SqlException sql_ex)
+            {
+                // Если произошла ошибка при закрытии соединения, бросаем исключение с сообщением об ошибке
+                throw new Exception(sql_ex.Message);
+            }
+        }
+
+        // Метод для выполнения запроса без возвращаемого результата (асинхронная версия)
+        public void ExecuteNonQuery(string query)
+        {
+            // Если соединение не открыто, открываем его
+            if (sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                OpenConnection();
+            }
+
+            // Создаем SqlCommand для выполнения запроса
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            // Выполняем запрос
+            sqlCommand.ExecuteNonQuery();
+
+            // Закрываем соединение после выполнения запроса
+            CloseConnection();
+        }
+
+        // Метод для выполнения запроса с возвращаемым результатом (асинхронная версия)
+        //public async Task<List<MainWindow.Product>> ExecuteReaderProducts()
+        //{
+        //    if (sqlConnection.State != System.Data.ConnectionState.Open)
+        //    {
+        //        OpenConnection();
+        //    }
+        //    string query = "SELECT ID, ProductName, Quantity FROM [Products]";
+        //    List<MainWindow.Product> result = new List<MainWindow.Product>();
+
+        //    using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+        //    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            result.Add(new MainWindow.Product(
+        //                (int)reader[0],
+        //                reader[1].ToString(),
+        //                (int)reader[2]));
+        //        }
+        //    }
+        //    CloseConnection();
+        //    return result;
+        //}
+    }
+}
