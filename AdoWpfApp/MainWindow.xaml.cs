69266c56a93f476b53a4648c7e5c90f53990ab59
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-11-23 22:19:15 +0100

    1

--- AdoWpfApp/MainWindow.xaml.cs
+++ AdoWpfApp/MainWindow.xaml.cs
@@ -30,25 +30,19 @@ namespace AdoWpfApp
             {
                 MessageBox.Show("Connection opened failed!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
             await LoadData();
         }
-
         private async void ComboBoxQueries_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (ComboBoxQueries.SelectedItem == null)
                 return;
-
             string selectedQuery = (string)((ComboBoxItem)ComboBoxQueries.SelectedItem).Tag;
             DataTable resultTable = await databaseManager.ExecuteQuery(selectedQuery);
             DG_Table.ItemsSource = resultTable.DefaultView;
         }
-
         private async Task LoadData()
         {
             ComboBoxQueries.SelectedIndex = 0;
         }
-
-
     }
 }
